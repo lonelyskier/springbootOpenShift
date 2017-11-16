d7434e118ace81f4efa9a7554bfa5dd11f7f13a1
lonelyskier <dennis_lundberg@yahoo.se>
2017-11-16 15:53:29 +0100

    Initial

---
+++ src/main/java/se/dl/helloworld/springbootOpenShift/SpringbootOpenShiftApplication.java
@@ -0,0 +1,12 @@
+package se.dl.helloworld.springbootOpenShift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOpenShiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootOpenShiftApplication.class, args);
+	}
+}
