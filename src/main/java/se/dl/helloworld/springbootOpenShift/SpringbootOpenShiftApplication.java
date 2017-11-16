38b68fe4d8cba4480ccefa53970f9be3b49a2d8d
lonelyskier <dennis_lundberg@yahoo.se>
2017-11-16 16:13:32 +0100

    Spring Boot with first controller.

--- src/main/java/se/dl/helloworld/springbootOpenShift/SpringbootOpenShiftApplication.java
+++ src/main/java/se/dl/helloworld/springbootOpenShift/SpringbootOpenShiftApplication.java
@@ -2,11 +2,22 @@ package se.dl.helloworld.springbootOpenShift;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpringbootOpenShiftApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootOpenShiftApplication.class, args);
+	}
+}
+
+@RestController
+class HomeController {
+
+	@GetMapping("/")
+	public String test(){
+		return"HelloWorld!";
 	}
 }
